95535d046b26270f10ecf8275d7e1f214aee6321
Tulun <jaykiraly@gmail.com>
2016-03-31 11:32:32 -0700

    Default index.js page loads.

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -21,10 +21,13 @@ module.exports = {
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
   ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
   module: {
     loaders: [
       {
-        test: /\.js$/,
+        test: /\.js|\.jsx$/,
         loaders: ['babel'],
         include: path.join(__dirname, 'src')
       },
