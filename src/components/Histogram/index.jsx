88856f9e8d716a299ef1ddc4ce01a8438cf6643c
Tulun <jaykiraly@gmail.com>
2016-04-01 10:25:50 -0700

    Histogram renders.

--- src/components/Histogram/index.jsx
+++ src/components/Histogram/index.jsx
@@ -1,32 +1,3 @@
-import React, { Component } from 'react';
-import d3 from 'd3';
-
-class Histogram extends Component {
-  constructor(props) {
-    super();
-
-    this.histogram = d3.layout.histogram();
-    this.widthScale = d3.scale.linear();
-    this.yScale = d3.scale.linear();
-
-    this.update_d3(props);
-  }
-
-  componentWillReceiveProps(newProps) {
-    this.update_d3(newProps)
-  }
-
-  update_d3(props) {
-    
-  }
-  render() {
-    let translate = 'translate(0, ${this.props.topMargin})';
-
-    return (
-      <g className='histogram' transform={translate}>
-      </g>
-    );
-  }
-}
+import Histogram from './Histogram';
 
 export default Histogram;
